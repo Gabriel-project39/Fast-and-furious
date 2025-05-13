914cd88e663abbb368a29e7f93899dd8c26d9cd7
Gabriel-project39 <gabrielkimanthi39@gmail.com>
2025-05-13 22:53:21 +0300

    Main

--- index.js
+++ index.js
@@ -11,3 +11,20 @@ toggleBtn.addEventListener("click", () => {
         toggleBtn.textContent = "🌞"; // Light mode
     }
 });
+let currentIndex = 0;
+const slides = document.querySelector('.swiper-slide');
+const totalSlides = document.querySelectorAll('.image-slide').length;
+
+function showSlide(index) {
+  slides.style.transform = `translateX(-${index * 100}%)`;
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % totalSlides; // loop back to 0
+  showSlide(currentIndex);
+}
+
+setInterval(nextSlide, 4000); // Change slide every 4 seconds
+
+// Show first slide on load
+showSlide(currentIndex);
