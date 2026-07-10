fd110886ca0063cf728e9ec77581efff879e90b1
Gabriel Kimanthi <gabrielkimanthi39@gmail.com>
2026-07-10 18:11:48 +0300

    about

--- script.js
+++ script.js
@@ -238,3 +238,13 @@ if (testimonialTrack) {
     startTestimonials();
 
 }
+
+
+const menu = document.querySelector(".menu-toggle");
+const nav = document.querySelector(".nav-links");
+
+menu.addEventListener("click", ()=>{
+
+    nav.classList.toggle("active");
+
+});
